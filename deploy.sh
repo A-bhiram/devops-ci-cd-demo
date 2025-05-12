528d7b136355932ad2b9bdac9ffddee0832b2b33
A-bhiram <abhirammanojofficial@gmail.com>
2025-05-12 08:25:59 +0530

    Add deploy script for GitHub CD pipeline

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Stop and remove the old container (if it's already running)
+docker stop flask-container || true
+docker rm flask-container || true
+
+# Pull the latest Docker image from Docker Hub
+docker pull abhirammanoj/flask-ecom:latest
+
+# Run the updated container on port 80
+docker run -d --name flask-container -p 80:5000 abhirammanoj/flask-ecom:latest
