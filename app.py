c0a31b93857a822d4b8e468da9b629a8e9485f1e
A-bhiram <abhirammanojofficial@gmail.com>
2025-05-13 10:38:48 +0530

    Test CI/CD: update homepage message

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from DevOps CI/CD project!"
+   return "DevOps update: auto-deploy working!"
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
